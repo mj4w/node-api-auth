09daaa98be413fea669a47c937035f755ff183f6
mj4w <marcelaribal963@gmail.com>
2024-02-25 19:33:38 +0800

    new changes

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const morgan = require('morgan') 
+const createError = require('http-errors')
+require('dotenv').config()
+
+const AuthRoute = require('./Routes/Auth')
+const app = express()
+
+app.get('/',async(req,res,next) => {
+    res.send('Welcome! Auth API')
+})
+
+// Developer server
+app.use(morgan('dev'))
+
+// Auth Route
+app.use('/auth',AuthRoute)
+
+app.use(async(req,res,next) => {
+    next(createError.NotFound()) // using http-errors
+    // next function trigger and pass to other functions
+})
+
+app.use((error,req,res,next) => {
+    res.status(error.status || 500)
+    res.send({
+        error: {
+            status: error.status || 500,
+            message: error.message
+        }
+    })
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
